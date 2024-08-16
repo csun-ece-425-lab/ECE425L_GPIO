5e22e8523074fcaf0810aba5f2b9c2eb0661cd8f
ananas-csun <ananas.ece25@gmail.com>
2024-08-16 10:37:40 -0700

    Added template for GPIO lab

---
+++ GPIO/main.c
@@ -0,0 +1,29 @@
+/**
+ * @file main.c
+ *
+ * @brief Main source code for the GPIO program.
+ *
+ * This file contains the main entry point and function definitions for the GPIO program.
+ * It interfaces with the following:
+ *  - User LED (RGB) Tiva C Series TM4C123G LaunchPad
+ *	- EduBase Board LEDs (LED0 - LED3)
+ *	- EduBase Board Push Buttons (SW2 - SW5)
+ *
+ * To verify the pinout of the user LED, refer to the Tiva C Series TM4C123G LaunchPad User's Guide
+ * Link: https://www.ti.com/lit/pdf/spmu296
+ *
+ * @author Aaron Nanas
+ */
+ 
+#include "TM4C123GH6PM.h"
+#include "SysTick_Delay.h"
+#include "GPIO.h"
+
+int main(void)
+{
+
+	while(1)
+	{
+
+	}
+}
